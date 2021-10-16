7fe4db13bc009b2d44cbf74b165053980d6f2826
xmNuc <xmnuc@o2.pl>
2021-10-16 12:46:34 +0000

    update calc.js

--- routes/calc.js
+++ routes/calc.js
@@ -5,7 +5,17 @@ const calcRouter = express.Router();
 calcRouter.post('/check', (req, res) => {
   const { numberA, numberB } = req.body;
 
-  console.log(numberA, numberB);
+  let divider;
+
+  if (numberA % numberB === 0) {
+    answer = { divider: true };
+  } else {
+    answer = { divider: false };
+  }
+
+  res.json(answer);
+
+  // console.log(numberA, numberB);
 
   res.json({ ok: 'Json is working' });
 });
