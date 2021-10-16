48d8d891746acca9c91d87481f7a64b89dad25d1
xmNuc <xmnuc@o2.pl>
2021-10-16 00:11:07 +0000

    first commit

---
+++ public/js/front.js
@@ -0,0 +1,27 @@
+const numberAInput = document.querySelector('#number-a');
+const numberBInput = document.querySelector('#number-b');
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const numberA = Number(numberAInput);
+  const numberB = Number(numberBInput);
+
+  console.log(numberA, numberB);
+
+  const res = fetch('/calc/check', {
+    method: 'POST',
+    body: JSON.stringify({
+      numberA,
+      NumberB,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = await res.json();
+
+  console.log(data);
+});
